a20806c1b5df792d7be88ea776772b4e9b0eda27
qloog <quanlongwang@gmail.com>
2015-10-14 21:34:15 +0800

    remove vendor from gitignore

--- database/migrations/2015_07_20_074010_create_users_table.php
+++ database/migrations/2015_07_20_074010_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
                 $table->increments('id');
                 $table->string('username')->unique();
-                $table->string('realname')->default('');
                 $table->string('email')->unique();
                 $table->string('password', 60);
                 $table->tinyInteger('status')->default(1);
