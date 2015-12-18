3e81a35191c98f516703390ef0af6d10d1d9324b
qloog <quanlongwang@gmail.com>
2015-12-18 15:56:09 +0800

    new add vender:LogViewer

--- config/app.php
+++ config/app.php
@@ -151,7 +151,7 @@ return [
         LucaDegasperi\OAuth2Server\Storage\FluentStorageServiceProvider::class,
         LucaDegasperi\OAuth2Server\OAuth2ServerServiceProvider::class,
         Dingo\Api\Provider\LaravelServiceProvider::class,
-
+        Arcanedev\LogViewer\LogViewerServiceProvider::class
 
     ],
     /*
