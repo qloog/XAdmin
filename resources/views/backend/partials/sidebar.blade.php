8a808784381a9d8fdadc47eef316bfae081ef621
qloog <quanlongwang@gmail.com>
2015-12-18 16:18:37 +0800

    add system menu in sidebar

--- resources/views/backend/partials/sidebar.blade.php
+++ resources/views/backend/partials/sidebar.blade.php
@@ -78,6 +78,33 @@
                         </ul>
                     </li>
 
+                    <li @if(Request::is('admin/system/*')) class="active open" @endif>
+                        <a href="javascript:;" class="dropdown-toggle">
+                            <i class="menu-icon fa fa-newspaper-o"></i>
+                            <span class="menu-text"> 系统管理 </span>
+                            <b class="arrow fa fa-angle-down"></b>
+                        </a>
+
+                        <b class="arrow"></b>
+
+                        <ul class="submenu">
+                            <li>
+                                <a href="{{ url('admin/log-viewer') }}" target="_blank">
+                                    <i class="menu-icon fa fa-caret-right"></i>
+                                    Log Viewer
+                                </a>
+                                <b class="arrow"></b>
+                            </li>
+                            <li @if(Request::is('admin/news/category')) class="active" @endif>
+                                <a href="javascript:;">
+                                    <i class="menu-icon fa fa-caret-right"></i>
+                                    待增加
+                                </a>
+                                <b class="arrow"></b>
+                            </li>
+                        </ul>
+                    </li>
+
                     <li @if(Request::is('admin/news*')) class="active open" @endif>
                         <a href="javascript:;" class="dropdown-toggle">
                             <i class="menu-icon fa fa-newspaper-o"></i>
