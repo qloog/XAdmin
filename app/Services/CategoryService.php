11c8dd40210ff66b1c125d6f171a77bbfbeeae33
qloog <quanlongwang@gmail.com>
2016-08-19 10:48:34 +0800

    format code

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -2,19 +2,22 @@
 
 namespace App\Services;
 
-class CategoryService {
+class CategoryService
+{
 
     /**
      * 组合一维数组
+     *
      * @param array  $cate
      * @param string $html
      * @param int    $pid
      * @param int    $level
      * @return array
      */
-    public static function unlimitedForLevel($cate, $html = '--', $pid = 0, $level = 0) {
+    public static function unlimitedForLevel($cate, $html = '--', $pid = 0, $level = 0)
+    {
         $arr = array();
-        foreach($cate as $k => $v) {
+        foreach ($cate as $k => $v) {
             if ($v['pid'] == $pid) {
                 $v['level'] = $level + 1;
                 $v['html'] = str_repeat($html, $level);
@@ -22,60 +25,70 @@ class CategoryService {
                 $arr = array_merge($arr, self::unlimitedForLevel($cate, $html, $v['id'], $level + 1));
             }
         }
+
         return $arr;
     }
 
     /**
      * 组合多维数组
+     *
      * @param        $cate
      * @param string $name
      * @param int    $pid
      * @return array
      */
-	public static function unlimitedForLayer($cate, $name='child', $pid=0) {
-		$arr = array();
-		foreach ($cate as $v) {
-			if ($v['pid'] == $pid) {
-				$v[$name] = self::unlimitedForLayer($cate, $name, $v['id']);
-				$arr[] = $v;
-			}
-		}
-		return $arr;
-	}
+    public static function unlimitedForLayer($cate, $name = 'child', $pid = 0)
+    {
+        $arr = array();
+        foreach ($cate as $v) {
+            if ($v['pid'] == $pid) {
+                $v[$name] = self::unlimitedForLayer($cate, $name, $v['id']);
+                $arr[] = $v;
+            }
+        }
 
-	/**
-	 * 根据一个子分类id获取所有的父级分类
-	 * eg:  首页>衣服>女装>外套
-     * @param array $cate	分类数组
-     * @param int   $id		当前分类id
+        return $arr;
+    }
+
+    /**
+     * 根据一个子分类id获取所有的父级分类
+     * eg:  首页>衣服>女装>外套
+     *
+     * @param array $cate 分类数组
+     * @param int   $id   当前分类id
      * @return array
      */
-	public static function getParents($cate, $id) {
-		 $arr = array();
-		 foreach($cate as $v) {
-			 if ($v['id'] == $id) {
-				 $arr[] = $v;
-				 $arr = array_merge(self::getParents($cate, $v['pid']), $arr);
-			 }
-		 }
-		 return $arr;
-	 }
+    public static function getParents($cate, $id)
+    {
+        $arr = array();
+        foreach ($cate as $v) {
+            if ($v['id'] == $id) {
+                $arr[] = $v;
+                $arr = array_merge(self::getParents($cate, $v['pid']), $arr);
+            }
+        }
+
+        return $arr;
+    }
 
     /**
      * 根据父级分类id获得所有子分类id
+     *
      * @param     $cate
      * @param int $pid
      * @return array
      */
-	public static function getChilds($cate, $pid = 0) {
-		$arr = array();
-		foreach($cate as $v) {
-			if ($v['pid'] == $pid) {
-				$arr[] = $v;
-				$arr = array_merge($arr, self::getChilds($cate, $v['id']));
-			}
-		}
-		return $arr;
-	}
+    public static function getChilds($cate, $pid = 0)
+    {
+        $arr = array();
+        foreach ($cate as $v) {
+            if ($v['pid'] == $pid) {
+                $arr[] = $v;
+                $arr = array_merge($arr, self::getChilds($cate, $v['id']));
+            }
+        }
+
+        return $arr;
+    }
 }
 
