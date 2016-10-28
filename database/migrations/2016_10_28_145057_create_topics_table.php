8cc28803a9f14c922d04d8de5873d5d90b6f51e8
qloog <quanlongwang@gmail.com>
2016-10-28 16:54:13 +0800

    alter topic table

--- database/migrations/2016_10_28_145057_create_topics_table.php
+++ database/migrations/2016_10_28_145057_create_topics_table.php
@@ -17,9 +17,9 @@ class CreateForumTopicsTable extends Migration
             $table->increments('id');
             $table->string('title')->comment('标题');
             $table->text('body')->comment('帖子内容');
-            $table->integer('view_count')->default(0)->comment('浏览数');
-            $table->integer('reply_count')->default(0)->comment('回复数');
-            $table->integer('vote_count')->default(0)->comment('投票数');
+            $table->integer('view_count')->unsigned()->default(0)->comment('浏览数');
+            $table->integer('reply_count')->unsigned()->default(0)->comment('回复数');
+            $table->integer('vote_count')->unsigned()->default(0)->comment('投票数');
             $table->enum('is_excellent', ['yes', 'no'])->default('no')->comment('是否加精帖子');
             $table->enum('is_blocked', ['yes', 'no'])->default('no')->comment('是否block帖子');
             $table->integer('last_reply_user_id')->unsigned()->default(0)->index()->comment('最后回复用户');
