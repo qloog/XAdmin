b4b99ef91cde20967b8764f8d682cd3259b6416b
qloog <quanlongwang@gmail.com>
2016-11-10 10:58:57 +0800

    add table forum_topics

--- database/migrations/2016_10_24_190844_forum_setup_tables.php
+++ database/migrations/2016_10_24_190844_forum_setup_tables.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ForumTopicsTables extends Migration
+class ForumSetupTables extends Migration
 {
     /**
      * Run the migrations.
@@ -13,6 +13,22 @@ class ForumTopicsTables extends Migration
      */
     public function up()
     {
+        Schema::create('forum_topics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->comment('标题');
+            $table->text('body')->comment('帖子内容');
+            $table->integer('view_count')->unsigned()->default(0)->comment('浏览数');
+            $table->integer('reply_count')->unsigned()->default(0)->comment('回复数');
+            $table->integer('vote_count')->unsigned()->default(0)->comment('投票数');
+            $table->enum('is_excellent', ['yes', 'no'])->default('no')->comment('是否加精帖子');
+            $table->enum('is_blocked', ['yes', 'no'])->default('no')->comment('是否block帖子');
+            $table->integer('last_reply_user_id')->unsigned()->default(0)->index()->comment('最后回复用户');
+            $table->string('source')->default('')->comment('访问来源 iOS，Android, PC');
+            $table->integer('user_id')->unsigned()->default(0)->index()->comment('创建者id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
         Schema::create('forum_categories', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('parent_id')->unsigned()->default(0)->comment('父级id');
@@ -47,8 +63,8 @@ class ForumTopicsTables extends Migration
      */
     public function down()
     {
-        Schema::drop('forum_categories');
         Schema::drop('forum_topics');
+        Schema::drop('forum_categories');
         Schema::drop('forum_replies');
     }
 }
